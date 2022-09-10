676edca2fc4225a4f10af447babc20422990848c
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2022-09-10 17:31:18 -0500

    viewchild 43

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { Persona } from '../persona.model'; //importamos clase Persona
 
 @Component({
@@ -12,9 +12,11 @@ export class FormularioComponent {
 
   //nombreInput:string = ''; se quita en clase 42, pues no se utilizará two way bidding 
   //apellidoInput:string = '';
+  @ViewChild('nombreInput') nombreInput: ElementRef;  //se importa este decorador, se pone como parámetro el nombre de la referencia nombreInput y se le asigna a una variable tipo ElementRef
+  @ViewChild('apellidoInput') apellidoInput: ElementRef; 
 
-  agregarPersona(nombreInput:HTMLInputElement, apellidoInput:HTMLInputElement){ //Clase 42, recibe 2 parámetros #nombreInput, #apellidoInput
-    let persona1 = new Persona(nombreInput.value, apellidoInput.value); //nombreInput y apellidoInput, guarda la información que se haya escrito en el campo de texto, clase 42, se elimina this. y agregamos . al final, ya que no son atributos de la clase sino parámetros del método-función
+  agregarPersona(){ //Clase 43, se eliminan los dos parámetros
+    let persona1 = new Persona(this.nombreInput.nativeElement.value, this.apellidoInput.nativeElement.value); //usamos this. porque son de la clase, y colocamos  .nativeElement.value para rescatar su valor
     //this.personas.push(persona1); //ingresamos el objeto al arreglo de personas Persona[], no se puede acceder al arreglo personas desde Component hijo Form, se hará de otra manera
     this.personaCreada.emit(persona1);  //objeto tipo Persona que queremos propagar de componente hijo a componente padre, todo esto reemplaza línea anterior
   }
