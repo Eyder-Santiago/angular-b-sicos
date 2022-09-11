535b41ae2dbb48d478d370ef4cd422eaad9c813a
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2022-09-11 17:38:41 -0500

    Quitando boottrap, two way biding again -48

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -11,17 +11,19 @@ import { PersonasService } from '../personas.service';
 })
 export class FormularioComponent {
  
-  //nombreInput:string = ''; se quita en clase 42, pues no se utilizará two way bidding 
-  //apellidoInput:string = '';
+  nombreInput:string = ''; //se habilita en clase 48 para usar two way bidding 
+  apellidoInput:string = '';
+
+  /*
   @ViewChild('nombreInput') nombreInput: ElementRef;  //se importa este decorador, se pone como parámetro el nombre de la referencia nombreInput y se le asigna a una variable tipo ElementRef
   @ViewChild('apellidoInput') apellidoInput: ElementRef; 
-
+*/
   constructor(private loggingService:LoggingService, private personasService : PersonasService){ //los servicios se inyectan al constructor usando Dependency Injection, clase 45 se importa el servicio de Personas
     this.personasService.saludar.subscribe((indice: number) => alert("El indice es: " + indice)); //subscribiéndose al evento que se está emitiendo saludar es emiter
   } 
 
   agregarPersona(){ //Clase 43, se eliminan los dos parámetros
-    let persona1 = new Persona(this.nombreInput.nativeElement.value, this.apellidoInput.nativeElement.value); //usamos this. porque son de la clase, y colocamos  .nativeElement.value para rescatar su valor
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput); 
    // this.loggingService.enviaMensajeAConsola("Enviamos persona con nombre:" + persona1.nombre +" apellido:" + persona1.apellido);
     //this.personaCreada.emit(persona1);  //clase 45, ya no se necesita emitir porque se está trabajando directamente desde un servicio, se harán en su lugar las líneas inferiores
     this.personasService.agregarPersona(persona1); //pasamos el objeto de tipo Persona
