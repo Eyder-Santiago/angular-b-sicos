73105f118fa36a3256d71e20964c0f4dc95f7ed0
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2022-09-21 12:56:15 -0500

    redirección y navegación en agregar persona -65

--- src/app/personas/formulario/formulario.component.ts
+++ src/app/personas/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'; //clase 45 se eleimina Output y EventEmitter pues no se van a utilizar porque se usará un servicio
+import { Router } from '@angular/router';
 import { LoggingService } from '../../loggingService.service'; //lo añade automáticamente ángular usando dependency injection, ya se importó globalmente en app.module.ts, no hay necesidad de importarlo por clase
 import { Persona } from '../../persona.model'; //importamos clase Persona
 import { PersonasService } from '../../personas.service';
@@ -18,15 +19,17 @@ export class FormularioComponent {
   @ViewChild('nombreInput') nombreInput: ElementRef;  //se importa este decorador, se pone como parámetro el nombre de la referencia nombreInput y se le asigna a una variable tipo ElementRef
   @ViewChild('apellidoInput') apellidoInput: ElementRef; 
 */
-  constructor(private loggingService:LoggingService, private personasService : PersonasService){ //los servicios se inyectan al constructor usando Dependency Injection, clase 45 se importa el servicio de Personas
+  constructor(private loggingService:LoggingService, private personasService : PersonasService, private router:Router){ //los servicios se inyectan al constructor usando Dependency Injection, clase 45 se importa el servicio de Personas
     this.personasService.saludar.subscribe((indice: number) => alert("El indice es: " + indice)); //subscribiéndose al evento que se está emitiendo saludar es emiter
   } 
 
-  agregarPersona(){ //Clase 43, se eliminan los dos parámetros
+  onGuardarPersona(){ //Clase 43, se eliminan los dos parámetros
     let persona1 = new Persona(this.nombreInput, this.apellidoInput); 
    // this.loggingService.enviaMensajeAConsola("Enviamos persona con nombre:" + persona1.nombre +" apellido:" + persona1.apellido);
     //this.personaCreada.emit(persona1);  //clase 45, ya no se necesita emitir porque se está trabajando directamente desde un servicio, se harán en su lugar las líneas inferiores
     this.personasService.agregarPersona(persona1); //pasamos el objeto de tipo Persona
+    //clase 64 rutas, al dar clic y que a la vez se ejecuta este método onGuardarPersona, nos redirigirá a la ruta personas
+    this.router.navigate(['personas']);
   }
 
 }
--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { LoggingService } from '../loggingService.service';
 import { Persona } from '../persona.model';
 import { PersonasService } from '../personas.service';
@@ -21,11 +22,18 @@ export class PersonasComponent implements OnInit {
   */  
 
       //servicio LoggingServices
-    constructor(private loggingService: LoggingService, private personasServices: PersonasService){
-
-    }
+    constructor(private loggingService: LoggingService, 
+      private personasServices: PersonasService,
+      private router:Router
+    ){}
   ngOnInit(): void { //clase 45, este método se ejecuta despupes del contructor
     this.personas = this.personasServices.personas; //valor inicial arreglo vaacío línea 16, después se inicializa el método ngOnInit y se inicializa la variable this.personas con el arreglo definido en PersonasService, this.personas.Service.personas
+  }
+
+  agregar(){
+    //clase router, servicio
+    //se dirige a la ruta personas/agregar
+      this.router.navigate(['personas/agregar']);
   }
 
 }
