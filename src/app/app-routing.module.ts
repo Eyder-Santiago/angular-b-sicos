a0ab0ec37c50e0b93daa498b63cf5063cb5820d0
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2022-09-21 12:05:39 -0500

    routing module -64

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioComponent } from './personas/formulario/formulario.component';
+import { PersonasComponent } from './personas/personas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:PersonasComponent}, //cuando estemos en inicio se va a desplegar el componente personas
+  {path: 'personas', component: PersonasComponent},
+  {path: 'personas/agregar', component: FormularioComponent}, //cuando agreguemos personas se va a mostrar el componente de formulario
+  {path: 'personas/:id', component: FormularioComponent}, //nombre parámetro 'id', elque va a procesar el url va a hacer formularioComponent
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
